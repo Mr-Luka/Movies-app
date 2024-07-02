a64cc8f44c7b8fae99a678b4cb3cc060ff8aeeae
Luka <luka.luka.luka93@gmail.com>
2024-07-02 16:50:04 -0700

    selected the elements I will be working with

---
+++ script.js
@@ -0,0 +1,4 @@
+const movieSearchBox = document.querySelector("#movie-search-box");
+const searchList = document.querySelector("#search-list");
+const resultGrid = document.querySelector("#result-grid");
+
