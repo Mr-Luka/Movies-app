936b5b9d1e30608725ef3689c03491e4c9754411
Luka <luka.luka.luka93@gmail.com>
2024-07-02 16:56:36 -0700

    made async function thats gonna load movies from the API

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const movieSearchBox = document.querySelector("#movie-search-box");
 const searchList = document.querySelector("#search-list");
 const resultGrid = document.querySelector("#result-grid");
 
+//  load movies from API
+async function loadMovies(searchTerm) {
+    const URL = `http://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=ac7d252c`
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if(data.Response === "True") displayMovieList(data.Search);
+}
+loadMovies("lord of the rings")
